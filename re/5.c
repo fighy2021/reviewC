a02806007f7916e313d88de1888f33362362455e
fighy2021 <94731174+fighy2021@users.noreply.github.com>
2023-11-03 02:20:35 +0800

    Add files via upload

---
+++ re/5.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(void){
+	printf("Squareofnumber10is %d\n",10*10);
+	
+	system("pause");
+	return 0;
+	
+}
---
+++ re/First.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(void){
+	
+	printf("This is my very first C program!\n\n");	
+	
+	system("pause");
+	return 0;
+	
+}
